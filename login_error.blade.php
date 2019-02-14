6472d373a46139908d4269b4624467d046c6e018
Tran Van Tuan <45853151+tuantvcodegym@users.noreply.github.com>
2019-02-14 16:34:54 +0700

    Add files via upload

---
+++ login_error.blade.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TuanTran
+ * Date: 2/14/2019
+ * Time: 2:09 PM
+ */?>
+        <!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<p>sai ten dang nhap hoac mat khau</p>
+</body>
+</html>
